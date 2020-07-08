9e84e71400fc0e0d609e96e6476621ce49a94bb9
Craig Miller <craigmiller160@gmail.com>
2020-07-07 20:26:04 -0400

    Fixed broken tests

--- src/test/kotlin/io/craigmiller160/webutils/oauth2/OAuthConfigTest.kt
+++ src/test/kotlin/io/craigmiller160/webutils/oauth2/OAuthConfigTest.kt
@@ -11,7 +11,7 @@ import org.mockito.Mockito.spy
 
 class OAuthConfigTest {
 
-    private val oAuthConfig = OAuthConfig()
+    private val oAuthConfig = OAuthConfig(jwkPath = "/path")
 
     @Test
     fun test_loadJWKSet_firstTrySuccess() {
