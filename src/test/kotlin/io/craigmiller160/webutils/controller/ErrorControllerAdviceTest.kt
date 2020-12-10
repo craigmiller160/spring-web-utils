b72591e48853b43aa2019de2cc390267e5d6c4ee
Craig Miller <craigmiller160@gmail.com>
2020-12-10 10:13:40 -0500

    Working on fixing broken tests

--- src/test/kotlin/io/craigmiller160/webutils/controller/ErrorControllerAdviceTest.kt
+++ src/test/kotlin/io/craigmiller160/webutils/controller/ErrorControllerAdviceTest.kt
@@ -18,14 +18,17 @@
 
 package io.craigmiller160.webutils.controller
 
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers
-import org.junit.jupiter.api.Assertions
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasProperty
+import org.hamcrest.Matchers.notNullValue
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.web.HttpMediaTypeNotSupportedException
 import org.springframework.web.HttpRequestMethodNotSupportedException
@@ -35,6 +38,8 @@ import org.springframework.security.access.AccessDeniedException
 @ExtendWith(MockitoExtension::class)
 class ErrorControllerAdviceTest {
 
+    // TODO add more tests for more methods
+
     @Mock
     private lateinit var req: HttpServletRequest
 
@@ -42,84 +47,89 @@ class ErrorControllerAdviceTest {
 
     @Test
     fun test_exception_withAnnotation() {
-        Mockito.`when`(req.requestURI).thenReturn("uri")
+        `when`(req.requestURI).thenReturn("uri")
+        `when`(req.method).thenReturn("GET")
         val ex = UnsupportedGrantTypeException("message")
 
         val error = errorControllerAdvice.exception(req, ex)
-        Assertions.assertEquals(400, error.statusCodeValue)
-        MatcherAssert.assertThat(error.body, CoreMatchers.allOf(
-                Matchers.hasProperty("status", CoreMatchers.equalTo(400)),
-                Matchers.hasProperty("error", CoreMatchers.equalTo("Bad Request")),
-                Matchers.hasProperty("message", CoreMatchers.equalTo("Unsupported OAuth2 Grant Type - message")),
-                Matchers.hasProperty("timestamp", CoreMatchers.notNullValue()),
-                Matchers.hasProperty("path", CoreMatchers.equalTo("uri"))
+        assertEquals(400, error.statusCodeValue)
+        assertThat(error.body, allOf(
+                hasProperty("status", equalTo(400)),
+                hasProperty("error", equalTo("Bad Request")),
+                hasProperty("message", equalTo("Unsupported OAuth2 Grant Type - message")),
+                hasProperty("timestamp", notNullValue()),
+                hasProperty("path", equalTo("uri"))
         ))
     }
 
     @Test
     fun test_exception() {
-        Mockito.`when`(req.requestURI).thenReturn("uri")
+        `when`(req.requestURI).thenReturn("uri")
+        `when`(req.method).thenReturn("GET")
         val ex = Exception("message")
 
         val error = errorControllerAdvice.exception(req, ex)
-        Assertions.assertEquals(500, error.statusCodeValue)
-        MatcherAssert.assertThat(error.body, CoreMatchers.allOf(
-                Matchers.hasProperty("status", CoreMatchers.equalTo(500)),
-                Matchers.hasProperty("error", CoreMatchers.equalTo("Internal Server Error")),
-                Matchers.hasProperty("message", CoreMatchers.equalTo("Error - message")),
-                Matchers.hasProperty("timestamp", CoreMatchers.notNullValue()),
-                Matchers.hasProperty("path", CoreMatchers.equalTo("uri"))
+        assertEquals(500, error.statusCodeValue)
+        assertThat(error.body, allOf(
+                hasProperty("status", equalTo(500)),
+                hasProperty("error", equalTo("Internal Server Error")),
+                hasProperty("message", equalTo("Error - message")),
+                hasProperty("timestamp", notNullValue()),
+                hasProperty("path", equalTo("uri"))
         ))
     }
 
     @Test
     fun test_accessDeniedException() {
-        Mockito.`when`(req.requestURI).thenReturn("uri")
-        val ex = Mockito.mock(AccessDeniedException::class.java)
-        Mockito.`when`(ex.message).thenReturn("message")
+        `when`(req.requestURI).thenReturn("uri")
+        `when`(req.method).thenReturn("GET")
+        val ex = mock(AccessDeniedException::class.java)
+        `when`(ex.message).thenReturn("message")
 
         val error = errorControllerAdvice.accessDeniedException(req, ex)
-        Assertions.assertEquals(403, error.statusCodeValue)
-        MatcherAssert.assertThat(error.body, CoreMatchers.allOf(
-                Matchers.hasProperty("status", CoreMatchers.equalTo(403)),
-                Matchers.hasProperty("error", CoreMatchers.equalTo("Access Denied")),
-                Matchers.hasProperty("message", CoreMatchers.equalTo("message")),
-                Matchers.hasProperty("timestamp", CoreMatchers.notNullValue()),
-                Matchers.hasProperty("path", CoreMatchers.equalTo("uri"))
+        assertEquals(403, error.statusCodeValue)
+        assertThat(error.body, allOf(
+                hasProperty("status", equalTo(403)),
+                hasProperty("error", equalTo("Access Denied")),
+                hasProperty("message", equalTo("message")),
+                hasProperty("timestamp", notNullValue()),
+                hasProperty("path", equalTo("uri"))
         ))
     }
 
     @Test
     fun test_mediaTypeNotSupportedException() {
-        Mockito.`when`(req.requestURI).thenReturn("uri")
-        val ex = Mockito.mock(HttpMediaTypeNotSupportedException::class.java)
-        Mockito.`when`(ex.message).thenReturn("message")
+        `when`(req.requestURI).thenReturn("uri")
+        `when`(req.method).thenReturn("GET")
+        val ex = mock(HttpMediaTypeNotSupportedException::class.java)
+        `when`(ex.message).thenReturn("message")
 
         val error = errorControllerAdvice.mediaTypeNotSupportedException(req, ex)
-        Assertions.assertEquals(415, error.statusCodeValue)
-        MatcherAssert.assertThat(error.body, CoreMatchers.allOf(
-                Matchers.hasProperty("status", CoreMatchers.equalTo(415)),
-                Matchers.hasProperty("error", CoreMatchers.equalTo("Unsupported Media Type")),
-                Matchers.hasProperty("message", CoreMatchers.equalTo("message")),
-                Matchers.hasProperty("timestamp", CoreMatchers.notNullValue()),
-                Matchers.hasProperty("path", CoreMatchers.equalTo("uri"))
+        assertEquals(415, error.statusCodeValue)
+        assertThat(error.body, allOf(
+                hasProperty("status", equalTo(415)),
+                hasProperty("error", equalTo("Unsupported Media Type")),
+                hasProperty("message", equalTo("message")),
+                hasProperty("timestamp", notNullValue()),
+                hasProperty("path", equalTo("uri"))
         ))
     }
 
     @Test
     fun test_methodNotSupportedException() {
-        Mockito.`when`(req.requestURI).thenReturn("uri")
-        val ex = Mockito.mock(HttpRequestMethodNotSupportedException::class.java)
-        Mockito.`when`(ex.message).thenReturn("message")
+        `when`(req.requestURI).thenReturn("uri")
+        `when`(req.method).thenReturn("GET")
+        val ex = mock(HttpRequestMethodNotSupportedException::class.java)
+        `when`(ex.message).thenReturn("message")
 
         val error = errorControllerAdvice.methodNotSupportedException(req, ex)
-        Assertions.assertEquals(405, error.statusCodeValue)
-        MatcherAssert.assertThat(error.body, CoreMatchers.allOf(
-                Matchers.hasProperty("status", CoreMatchers.equalTo(405)),
-                Matchers.hasProperty("error", CoreMatchers.equalTo("Method Not Allowed")),
-                Matchers.hasProperty("message", CoreMatchers.equalTo("message")),
-                Matchers.hasProperty("timestamp", CoreMatchers.notNullValue()),
-                Matchers.hasProperty("path", CoreMatchers.equalTo("uri"))
+        assertEquals(405, error.statusCodeValue)
+        assertThat(error.body, allOf(
+                hasProperty("status", equalTo(405)),
+                hasProperty("error", equalTo("Method Not Allowed")),
+                hasProperty("message", equalTo("message")),
+                hasProperty("timestamp", notNullValue()),
+                hasProperty("path", equalTo("uri"))
         ))
     }
 
